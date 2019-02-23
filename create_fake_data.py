1e8994f9c4ffa70966bdb0d851811a0f3d610859
Ming Qin <mingqin1@gmail.com>
2019-02-22 16:33:51 -0800

    Add files via upload

---
+++ create_fake_data.py
@@ -0,0 +1,43 @@
+from datetime import timedelta, date, datetime
+import random
+import string
+import requests
+import json
+
+
+
+r = requests.get('https://api.github.com/events')
+print r.content
+print "Hello, world!"
+
+start_date = datetime(2019, 2, 20)
+
+for x in range(60*1):
+
+    print x, "," ,
+
+    single_date = start_date + timedelta(minutes=x)
+    #print ( single_date.isoformat())
+
+    dt = single_date.strftime("%Y-%m-%d")
+    
+    #print ( dt)
+    
+    url = "https://elastic:j8J6xNdrujtr560tUhPpJIPh@48bc81b663c64e919e8045ccb4bcbd4d.us-west1.gcp.cloud.es.io:9243/{}-fake/fake".format(dt)
+    
+    #print url
+    
+    doc = { "user" : "patriotism.com", 
+            "post_date" : single_date.isoformat(),  
+            "message" : ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(12)) , 
+            "x": x,
+            "cpu": random.randint(0,100),
+            "io": random.randint(0,100)
+            
+    }
+    
+    print  json.dumps(doc)
+    
+    resp = requests.post(url, json=doc)
+    
+    print resp.text 
