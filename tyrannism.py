4afc6d5094eb3e55b60caddc8f46e5a08103245a
Ming Qin <mingqin1@gmail.com>
2019-02-26 14:38:47 -0800

    Update tyrannism.py

--- tyrannism.py
+++ tyrannism.py
@@ -1,1 +1,43 @@
+from datetime import timedelta, date, datetime
+import random
+import string
+import requests
+import json
 
+
+
+r = requests.get('https://api.github.com/events')
+print r.content
+print "Hello, world!"
+
+start_date = datetime(2019, 2, 21)
+
+for x in range(60*96):
+
+    print x, "," ,
+
+    single_date = start_date + timedelta(minutes=x)
+    #print ( single_date.isoformat())
+
+    dt = single_date.strftime("%Y-%m-%d")
+    
+    #print ( dt)
+    
+    url = "https://elastic:7FPxC9XsiEEotQD7y2JbvvT5@48bc81b663c64e919e8045ccb4bcbd4d.us-west1.gcp.cloud.es.io:9243/{}-fake/fake".format(dt)
+    
+    #print url
+    
+    doc = { "user" : "tyrannism.com", 
+            "post_date" : single_date.isoformat(),  
+            "message" : ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(12)) , 
+            "x": x,
+            "cpu": random.randint(0,100),
+            "io": random.randint(0,100)
+            
+    }
+    
+    print  json.dumps(doc)
+    
+    resp = requests.post(url, json=doc)
+    
+    print resp.text 
